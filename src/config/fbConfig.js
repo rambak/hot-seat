5f4bc26c6f6185d7c3d33082d91287667cc6d2b9
Rimma <rim23ma@gmail.com>
2019-03-14 16:47:22 -0500

    firebase redux

---
+++ src/config/fbConfig.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+var config = {
+  apiKey: "AIzaSyAwqfvXmK7JxJNjqTXcHq1pHaPcFby2VCQ",
+  authDomain: "hot-seat-444.firebaseapp.com",
+  databaseURL: "https://hot-seat-444.firebaseio.com",
+  projectId: "hot-seat-444",
+  storageBucket: "hot-seat-444.appspot.com",
+  messagingSenderId: "289259443681"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true})
+
+export default firebase;
