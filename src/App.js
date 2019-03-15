39fc5e1c695f62ec64b82e2a5b162127e24edec3
Rimma <rim23ma@gmail.com>
2019-03-15 14:18:23 -0500

    created homepage component with 2 buttons and wrote function that generate a pincode

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {HomePage} from './components/HomePage'
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path='/' component={HomePage}/>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/HomePage.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+import {
+  Button,
+  Container,
+  Header,
+} from 'semantic-ui-react'
+import { firestore } from 'firebase';
+import { combineReducers } from '../../../Library/Caches/typescript/3.3/node_modules/redux';
+
+
+
+const generatePinCode = () => {
+  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  const randomIdx = () => {
+    return Math.floor((Math.random() * alphabet.length) + 0)
+  }
+  let pincode=''
+  for (let i=0; i < 4; i++) {
+    pincode+=alphabet[randomIdx()]
+  }
+  return pincode
+}
+
+export class HomePage extends Component {
+
+  onClick {
+    set firestore as a new game with id as a PinCode
+    set props with pin code
+    gamePincode  combineReducers
+    go to /game/:PinCode
+  }
+
+
+  render() {
+    return (
+      <Container textAlign='center'>
+        <Header >Welcome, to Hot Seat Game!</Header>
+        <Button >Start a new Game</Button>
+        <Button>Enter a Game</Button>
+      </Container>
+    )
+  }
+}
