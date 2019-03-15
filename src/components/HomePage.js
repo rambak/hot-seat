e7ae4cf82861d2f2017c958019c24c7b72529b85
Rimma <rim23ma@gmail.com>
2019-03-15 14:42:18 -0500

    changes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,19 +9,24 @@ import { combineReducers } from '../../../Library/Caches/typescript/3.3/node_mod
 
 
 
-const generatePinCode = () => {
+const generate = () => {
   const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   const randomIdx = () => {
     return Math.floor((Math.random() * alphabet.length) + 0)
   }
   let pincode=''
-  for (let i=0; i < 4; i++) {
-    pincode+=alphabet[randomIdx()]
+  for (let i = 0; i < 4; i++) {
+    pincode += alphabet[randomIdx()]
   }
   return pincode
 }
 
 export class HomePage extends Component {
+
+  generatePinCode = () => {
+    const gamePinCode = generate();
+
+  }
 
   onClick {
     set firestore as a new game with id as a PinCode
@@ -35,7 +40,7 @@ export class HomePage extends Component {
     return (
       <Container textAlign='center'>
         <Header >Welcome, to Hot Seat Game!</Header>
-        <Button >Start a new Game</Button>
+        <Button onClick={() => {this.generatePinCode}}>Start a new Game</Button>
         <Button>Enter a Game</Button>
       </Container>
     )
