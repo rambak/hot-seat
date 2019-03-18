c785dc6b7314a92f7c78890390bf3f2173c1910d
Matti Barzilai <matti.barzilai@gmail.com>
2019-03-18 10:57:25 -0500

    add start game function to board00 component

--- src/components/00_WaitingForPlayers/Board00WaitingForPlayers.js
+++ src/components/00_WaitingForPlayers/Board00WaitingForPlayers.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Header, Button, List, Container } from 'semantic-ui-react';
+import { db } from '../../config/fbConfig';
 
 // const hardcodedPin = 'ABCD';
 
@@ -12,6 +13,30 @@ import { Header, Button, List, Container } from 'semantic-ui-react';
 const Board00WaitingForPlayers = props => {
   const pin = props.pin;
   const players = props.players;
+  const gameRef = props.gameRef;
+
+  const startGame = () => {
+    gameRef
+      .collection('players')
+      .get()
+      .then(function(players) {
+        const batch = db.batch();
+
+        players.docs.forEach(function(player, idx) {
+          batch.update(player.ref, {
+            score: 0,
+            turnOrder: idx,
+          });
+        });
+
+        batch.update(gameRef, {
+          inHotSeat: 0,
+          currentStage: 'upNow',
+        });
+
+        batch.commit();
+      });
+  };
 
   return (
     <Container textAlign="center">
@@ -25,7 +50,12 @@ const Board00WaitingForPlayers = props => {
           <List.Item>{player.name}</List.Item>
         ))}
       </List>
-      <Button>Start!</Button>
+      <Button
+        onClick={startGame}
+        // disabled={players.length < 3}
+      >
+        Start!
+      </Button>
     </Container>
   );
 };
