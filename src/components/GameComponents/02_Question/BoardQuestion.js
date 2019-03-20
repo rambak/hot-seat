bd0abedde5c724ff9b71f6cb6beb00805bf792c0
Anna Krause <anna.krause.cs@gmail.com>
2019-03-20 12:17:01 -0500

    inHotSeat refactored

--- src/components/GameComponents/02_Question/BoardQuestion.js
+++ src/components/GameComponents/02_Question/BoardQuestion.js
@@ -1,30 +1,32 @@
 import React from 'react';
-import { db } from '../../../config/fbConfig'
-
+import { db } from '../../../config/fbConfig';
 
 export class BoardQuestion extends React.Component {
   constructor() {
     super();
-    this.state={
-      question: ''
-    }
+    this.state = {
+      question: '',
+    };
   }
 
   async componentDidMount() {
-    let randomIdx
+    let randomIdx;
     do {
-      randomIdx = (Math.floor(Math.random() * 9 + 0)).toString();
-    } while (this.props.prevQuestions.randomIdx)
-    const curQuestion = await db.collection('questions').doc(randomIdx).get();
-    const question = curQuestion.data().question
-    this.setState({question})
-    this.props.prevQuestions[randomIdx] = true
+      randomIdx = Math.floor(Math.random() * 9 + 0).toString();
+    } while (this.props.prevQuestions.randomIdx);
+    const curQuestion = await db
+      .collection('questions')
+      .doc(randomIdx)
+      .get();
+    const question = curQuestion.data().question;
+    this.setState({ question });
+    this.props.prevQuestions[randomIdx] = true;
   }
 
   render() {
-    console.log('this.props.prevQuestions',this.props.prevQuestions)
-    if (this.state.question) return <div>{this.state.question}</div>
-    else return <div>loading</div>
+    console.log('this.props.prevQuestions', this.props.prevQuestions);
+    if (this.state.question) return <div>{this.state.question}</div>;
+    else return <div>loading</div>;
   }
 }
 
