aec4524c5adf428d2ed8f6bdcf1cdec4a3e19cc9
Anna Krause <anna.krause.cs@gmail.com>
2019-03-21 14:40:43 -0500

    current state

--- src/components/GameComponents/03_Voting/BoardVoting.js
+++ src/components/GameComponents/03_Voting/BoardVoting.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Header, Container } from 'semantic-ui-react';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { Timer } from '../../../utils/timer';
+// import { db } from '../../../config/fbConfig';
 
 export const BoardVoting = ({
   gameRef,
   currentQuestion,
   updateStage,
   inHotSeatName,
+  areVotesIn,
 }) => {
   const answersRef = gameRef.collection('answers');
   const answersCol = useCollection(answersRef);
@@ -20,6 +22,22 @@ export const BoardVoting = ({
       }
     });
   }
+
+  // if (areVotesIn) {
+  //   const correctAnswerVoters = gameRef
+  //     .collection('answers')
+  //     .doc(inHotSeatName)
+  //     .get()
+  //     .then(function(doc) {
+  //       return doc.playersVote;
+  //     });
+
+  //     const batch = db.runTransaction;
+  // correctAnswerVoters.forEach()
+  // batch.update();
+  // batch.commit();
+
+  // }
 
   return (
     <Container textAlign="center">
