52dc719c4c63c0581843c93e9fec4582c8fdaf9a
Matti Barzilai <matti.barzilai@gmail.com>
2019-03-21 11:22:27 -0500

    display results as card with stars for votes

--- src/components/GameComponents/02_Question/PlayerQuestion.js
+++ src/components/GameComponents/02_Question/PlayerQuestion.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button } from 'semantic-ui-react';
 
 export const PlayerQuestion = ({ name, gameRef }) => {
   const [answer, setAnswer] = useState('');
