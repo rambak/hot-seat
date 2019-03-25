3d8aafd12bd1a4fd3fe8f344ca388c2b61715ace
Matti Barzilai <45402179+mbarzilai@users.noreply.github.com>
2019-03-25 10:52:23 -0500

    Update PlayerGameOver.js
    Comment out deleting user at end of game

--- src/components/GameComponents/06_GameOver/PlayerGameOver.js
+++ src/components/GameComponents/06_GameOver/PlayerGameOver.js
@@ -3,12 +3,12 @@ import { Header } from 'semantic-ui-react';
 import { auth } from '../../../config/fbConfig';
 
 export const PlayerGameOver = props => {
-  const user = auth.currentUser;
-  if (user) {
-    user.delete().catch(function(error) {
-      console.log(error.message);
-    });
-  }
+//   const user = auth.currentUser;
+//   if (user) {
+//     user.delete().catch(function(error) {
+//       console.log(error.message);
+//     });
+//   }
   return (
     <Header className="title" textAlign="center" style={{ paddingTop: '35vh' }}>
       Game is over!
